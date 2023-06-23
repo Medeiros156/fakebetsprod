1b96827824a854f96e42eb618b4ae0d4640d4946
Pedro Medeiros <pedromedeiros156@hotmail.com>
2023-06-22 21:44:01 -0300

    csrf fix

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -23,7 +23,11 @@ export default function Login({ status, canResetPassword }) {
     const submit = (e) => {
         e.preventDefault();
 
-        post(route('login'));
+        const headers = {
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
+        };
+
+        post(route('login'), {}, { headers });
     };
 
     return (
