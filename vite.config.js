d079928911ba8b1f883718243b9d61da808ff333
Pedro Medeiros <pedromedeiros156@hotmail.com>
2023-06-22 21:17:06 -0300

    vitefix3

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,14 @@ import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  server: {
+    https: true,
+  },
   plugins: [
     laravel({
       input: "resources/js/app.jsx",
       refresh: true,
-      https: true,
+      // https: true,
     }),
     react(),
   ],
