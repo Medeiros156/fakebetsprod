d35dfb83a467a0bf3db549635297bedf5a7a7b01
Pedro Medeiros <pedromedeiros156@hotmail.com>
2023-06-22 21:36:08 -0300

    http fix

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -87,7 +87,7 @@ export default function Login({ status, canResetPassword }) {
                         </Link>
                     )}
                     <Link
-                        href={route('register')}
+                        to={route('register')}
                         className="ml-4 underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
                         Register
